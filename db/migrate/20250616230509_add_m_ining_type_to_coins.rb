86022eec82b03663426f01367d085f71cd942a24
Davidson <davidson.cg7@gmail.com>
2025-06-16 20:15:17 -0300

    Adding a standalone database, creating mining_type_id refference on coin tabla

---
+++ db/migrate/20250616230509_add_m_ining_type_to_coins.rb
@@ -0,0 +1,5 @@
+class AddMIningTypeToCoins < ActiveRecord::Migration[8.0]
+  def change
+    add_reference :coins, :mining_type, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_11_234408) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_16_230509) do
   create_table "coins", force: :cascade do |t|
     t.string "description"
     t.string "acronym"
     t.string "url_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "mining_type_id", null: false
+    t.index ["mining_type_id"], name: "index_coins_on_mining_type_id"
   end
 
   create_table "mining_types", force: :cascade do |t|
@@ -25,4 +27,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_11_234408) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "coins", "mining_types"
 end
