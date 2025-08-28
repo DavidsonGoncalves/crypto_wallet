71d2bf621921e3fe2ae6470d0f725aac10ba325a
Davidson <davidson.cg7@gmail.com>
2025-08-28 20:46:53 -0300

    Adjusting to receive the mining types by controller

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,6 @@
 class CoinsController < ApplicationController
   before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_mining_type_options, only: %i[ new edit create update ]
 
   # GET /coins or /coins.json
   def index
@@ -61,6 +62,10 @@ class CoinsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params.expect(:id))
+    end
+
+    def set_mining_type_options
+      @mining_type_options = MiningType.all.pluck(:description, :id)
     end
 
     # Only allow a list of trusted parameters through.
